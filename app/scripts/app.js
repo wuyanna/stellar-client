71be816a405b1dd637443fa8ef4786c671ca66ad
Yanna Wu <wuyanna2009@gmail.com>
2015-04-01 03:04:53 -0700

    remove hard code line

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -41,8 +41,8 @@ window.$get = function (dependency) {
 
 Omlet.ready(function() {
 
-  // var user = Omlet.scope.identity.account;
-  var user = "1aj054mh0n5bgq70a4dra71v3bntema4gs9icjjr8c6uhttqcekk";
+  var user = Omlet.scope.identity.account;
+
   var config = {
                 
                     type: 'federation',
